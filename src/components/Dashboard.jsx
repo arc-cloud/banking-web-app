0c78c3089c7714cc89b912eab5f47ad1bd1d2887
Charles Kamiri <charleskamiri6@gmail.com>
2022-03-31 21:43:37 +0300

    ...

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,37 @@
+import Welcome from './Welcome';
 import '../styles/Dashboard.css';
+
 
 const Dashboard = () => {
     return (
         <>
-        {/* Welcome text */}
-        <div className="custom_wrapper">
-            <div className="welcome_text">
-                <p className="welcome">Welcome!</p>
-                <p className="username">Charles Kamiri</p>
+            <div className="dash_wrapper">
+                <div className='sub_wrapper profile'>
+                    <img src={require('../picture.jpg')} alt="" />
+                </div>
+                <div className='sub_wrapper welcome'>
+                    <Welcome />
+                </div>
+                <div className='sub_wrapper main'>
+                    <div className="left">
+                        <div className="total">
+                            <p className="title">Total Balance</p>
+                            <div className="t_balance">
+                                <div className="ksh">KSh. 132,000</div>
+                                <div className="usd">$ 1148.83</div>
+                            </div>
+                        </div>
+                        <div className="specific">
+                            <p className="title">Accounts</p>
+                            <div className="accounts">
+                                
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className="right"></div>
+                </div>
             </div>
-
-            {/* Misc */}
-            <div className="misc"></div>
-
-            {/* Main content */}
-            <div className="main_content">
-                <div className="title">Account Information</div>
-                <div className="bank_account">Ksh. 40,000</div>
-                <div className="paypal">$75</div>
-                <div className="portfolio">Ksh. 18,000</div>
-            </div>
-
-            {/* Ads */}
-            <div className="ads"></div>
-        </div>
         </>
     )
 }
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Typed from "typed.js";
+
+const Welcome = () => {
+	// Create reference to store the DOM element containing the animation
+	const el = React.useRef(null);
+  // Create reference to store the Typed instance itself
+	const typed = React.useRef(null);
+
+  React.useEffect(() => {
+    const options = {
+    	strings: [
+        'Welcome, Charles'
+      ],
+      typeSpeed: 50,
+      backSpeed: 50,
+    };
+    
+    // elRef refers to the <span> rendered below
+    typed.current = new Typed(el.current, options);
+    
+    return () => {
+      // Make sure to destroy Typed instance during cleanup
+      // to prevent memory leaks
+      typed.current.destroy();
+    }
+  }, [])
+
+  return (
+    <div>
+      <div className="welcome_text">
+        <span style={{ whiteSpace: 'pre' }} ref={el} />
+      </div>
+    </div>
+  );
+}
+export default Welcome;

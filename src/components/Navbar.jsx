07f8b4409ea18aacb576b1b6af07364c522c94ca
Charles Kamiri <charleskamiri6@gmail.com>
2022-05-12 22:29:54 +0300

    ...

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            <div className="empty" id='empty'>
+            <div className="empty" id='empty' onClick={minimize_nav}>
 
             </div>
         </>
